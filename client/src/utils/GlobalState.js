2b0d4df97b88abee0bf2e502c5800c25919666f0
dit <ditazanelli@gmail.com>
2022-04-18 16:36:55 -0500

    global state obj implemented

---
+++ client/src/utils/GlobalState.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext } from "react";
+import { useProductReducer } from "./reducers";
+
+const StoreContext = createContext();
+// destructuring provider component from createContext Context obj
+const { Provider } = StoreContext;
+
+//instantiate our initial global state with the useProductReducer()
+const StoreProvider = ({ value = [], ...props }) => {
+  // returns : state (most up-to-date version of our global state object)  + dispatch (method we execute to update our state. looks for an action object passed in as its argument)
+  const [state, dispatch] = useProductReducer({
+    products: [],
+    categories: [],
+    currentCategory: "",
+  });
+  // use this to confirm it works!
+  // then return the StoreContext's <Provider> component with our state object and dispatch the function provided as data for the value prop.
+  console.log(state);
+  return <Provider value={[state, dispatch]} {...props} />;
+};
+
+// custom React Hook
+// any component that has access to our StoreProvider component can use any data in our global state container or update it using the dispatch function
+const useStoreContext = () => {
+  return useContext(StoreContext);
+};
+
+export { StoreProvider, useStoreContext };
---
+++ client/src/utils/actions.js
@@ -0,0 +1,7 @@
+
+// used by the productList component. we want to store the data retrieved for products by Apollo in this global state
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+// we want to take the list of categories retrieved from the server by Apollo and store it in this global state.
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+// we want to be able to select a category from the state created by the UPDATE_CATEGORIES action and display products for that category from the list we create from the UPDATE_PRODUCTS action.
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
